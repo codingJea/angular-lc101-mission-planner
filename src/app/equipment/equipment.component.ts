4e1036c49061f18279029b88acc11b93fbcca8c0
Jea Parker <codingjea@gmail.com>
2022-07-04 14:31:28 -0400

    Completed studio

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,3 +1,4 @@
+import { DomElementSchemaRegistry } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipmentList: string[] = [ 
+    "Habitat dome", "Drones", "Food containers", "Oxygen tanks"
+  ];
+
+  itemBeingEdited: string = null;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  addEquipment(equipmentName: string) {
+    this.equipmentList.push(equipmentName);
+
+  }
+
+  edit(item: string) {
+    this.itemBeingEdited = item;
+  }
+
+  save(updatedItem: string, item: string) {
+    let index = this.equipmentList.indexOf(item);
+    this.equipmentList[index] = updatedItem;
+    this.itemBeingEdited = null;
+  }
+
+  remove(item: string) {
+    let index = this.equipmentList.indexOf(item);
+    this.equipmentList.splice(index, 1);
+  }
+
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
+  experiments: string[] = [
+    "Mars soil sample", "Plant growth in habitat", "Human bone density"
+  ]
+
+  itemBeingEdited: string = null;
 
   constructor() { }
 
   ngOnInit() {
   }
+  add(item: string) {
+    this.experiments.push(item);
+  }
 
+  edit(item: string) {
+    this.itemBeingEdited = item;
+  }
+
+  save(updatedItem: string, item: string) {
+    let index = this.experiments.indexOf(item);
+    this.experiments[index] = updatedItem;
+    this.itemBeingEdited = null;
+  }
+
+  remove(item: string) {
+    let index = this.experiments.indexOf(item);
+    this.experiments.splice(index, 1);
+  }
 }
